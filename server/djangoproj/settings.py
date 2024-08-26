29864ad99f2d79335c4ad6a89f8c7faf6fa9463d
thombs <71855571+thombs@users.noreply.github.com>
2024-08-25 21:20:53 -0600

    Revert "Adding temporary changes to Github"
    This reverts commit af23863654f640d17497811453be321da643070c.

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -62,10 +62,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'frontend/static'),
-            os.path.join(BASE_DIR, 'frontend/build'),
-            os.path.join(BASE_DIR, 'frontend/build/static'),
-            ],
+            os.path.join(BASE_DIR,'frontend/static')
+        ],        'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
@@ -138,7 +136,5 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'frontend/static'),
-    os.path.join(BASE_DIR, 'frontend/build'),
-    os.path.join(BASE_DIR, 'frontend/build/static'),
+    os.path.join(BASE_DIR,'frontend/static')
 ]
