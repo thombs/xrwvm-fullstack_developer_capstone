22fb6532d09c2cc6f7c1792bd44831278c171686
Tim Hombs <timhombs@yahoo.com>
2024-09-02 15:30:51 -0400

    Adding temporary changes to Github

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -29,10 +29,10 @@ class CarModel(models.Model):
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(default=2023,
                                validators=[
-                               MaxValueValidator(2023),
-                               MinValueValidator(2015)
+                                MaxValueValidator(2023),
+                                MinValueValidator(2015)
                                ])
-    dealer_id = models.IntegerField(default=1)    
+    dealer_id = models.IntegerField(default=1)
     # Other fields as needed
 
     def __str__(self):
