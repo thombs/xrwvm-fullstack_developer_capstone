98ad004691195c698562f470fa4d6ce2d170f9fe
Tim Hombs <timhombs@yahoo.com>
2024-09-02 15:14:13 -0400

    Adding temporary changes to Github

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,8 +1,9 @@
 
 from django.db import models
-#from django.utils.timezone import now
+# from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
+
 
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
 class CarMake(models.Model):
@@ -10,14 +11,14 @@ class CarMake(models.Model):
     description = models.TextField()
     # Other fields as needed
 
-
     def __str__(self):
         return self.name  # Return the name as the string representation
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
+    # Many-to-One relationship
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
@@ -27,13 +28,12 @@ class CarModel(models.Model):
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(default=2023,
-        validators=[
-            MaxValueValidator(2023),
-            MinValueValidator(2015)
-        ])
+                                validators=[
+                                MaxValueValidator(2023),
+                                MinValueValidator(2015)
+                            ])
     dealer_id = models.IntegerField(default=1)    
     # Other fields as needed
-
 
     def __str__(self):
         return self.name  # Return the name as the string representation
